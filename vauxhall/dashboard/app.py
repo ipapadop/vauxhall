91bb228ec56196a87bb5678789e2927f0dd0d4b0
Yiannis Papadopoulos <giannis.papadopoulos@gmail.com>
2026-04-15 22:19:28 -0400

    chore: add SPDX copyright notices to all files

--- vauxhall/dashboard/app.py
+++ vauxhall/dashboard/app.py
@@ -1,3 +1,4 @@
+# SPDX-FileCopyrightText: 2026 Yiannis Papadopoulos <giannis.papadopoulos@gmail.com>
 # SPDX-License-Identifier: MIT
 
 """Main entry point for the Vauxhall Dashboard application."""

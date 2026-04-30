65b02b9ef91715e806388fbac712db0cad1b5ac1
Yiannis Papadopoulos <giannis.papadopoulos@gmail.com>
2026-04-30 16:13:40 -0400

    chore: final cleanup and verification

---
+++ scripts/test_visual_logging.py
@@ -0,0 +1,16 @@
+from vauxhall.core.logging import get_logger, setup_logging
+
+
+def test_visual_colors() -> None:
+    """Visually test the color output of the logger."""
+    setup_logging(level="DEBUG")
+    logger = get_logger("test_color")
+    logger.debug("This should be BLUE")
+    logger.info("This should be GREEN")
+    logger.warning("This should be YELLOW")
+    logger.error("This should be RED")
+    logger.critical("This should be BOLD RED")
+
+
+if __name__ == "__main__":
+    test_visual_colors()
